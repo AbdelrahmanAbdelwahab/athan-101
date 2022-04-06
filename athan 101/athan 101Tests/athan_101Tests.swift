dfdf192ee1ae25fe995bba3ebc7f61a45d2f7a74
abdelwahab <abdelrahman.abdelwahab@mooney.it>
2022-04-07 00:03:28 +0200

    athan 101 v0.0.0

---
+++ athan 101/athan 101Tests/athan_101Tests.swift
@@ -0,0 +1,33 @@
+//
+//  athan_101Tests.swift
+//  athan 101Tests
+//
+//  Created by ABDELWAHAB on 07/04/22.
+//
+
+import XCTest
+@testable import athan_101
+
+class athan_101Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ athan 101/athan 101UITests/athan_101UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  athan_101UITestsLaunchTests.swift
+//  athan 101UITests
+//
+//  Created by ABDELWAHAB on 07/04/22.
+//
+
+import XCTest
+
+class athan_101UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
